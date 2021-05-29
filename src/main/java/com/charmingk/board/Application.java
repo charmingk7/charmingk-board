ffee2e012abdd697946c6f3fd9da162c2c69aeae
developer <blue007m@naver.com>
2021-05-29 15:34:07 +0900

    게시판 페이지 제작 완료

--- src/main/java/com/charmingk/board/Application.java
+++ src/main/java/com/charmingk/board/Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing  //JPA Auditing 활성화
+//@EnableJpaAuditing  //JPA Auditing 활성화
+//@EnableJpaAuditing가 삭제됨
 //스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
 //이 클래스는 항상 프로젝트의 최상단에 위치해야 한다.
 //main 메소드에서 내장 WAS 실행
--- src/main/java/com/charmingk/board/web/IndexController.java
+++ src/main/java/com/charmingk/board/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.charmingk.board.web;
 
+import com.charmingk.board.config.auth.LoginUser;
+import com.charmingk.board.config.auth.dto.SessionUser;
 import com.charmingk.board.service.posts.PostsService;
 import com.charmingk.board.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,17 +10,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    //@LoginUser SessionUser user
+    //기존에 (User) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선되었다.
+    //이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 되었다.
+    public String index(Model model, @LoginUser SessionUser user) {
         //Model
         //서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
         //여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            //if(user != null)
+            //세션에 저장된 값이 있을 때만 model에 userName으로 등록
+            //세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 된다.
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
